3d9b07582e68e14376313a2d67fd5830dc32fc41
Bert Jansen <bjansen@microsoft.com>
2021-01-11 19:54:14 +0100

    Build script updates to support GA release

--- build/build-debug.ps1
+++ build/build-debug.ps1
@@ -1,21 +1,6 @@
 $versionIncrement = Get-Content "$PSScriptRoot\version.debug.increment" -Raw
-$versionIncrement = $versionIncrement -as [int]
-$versionIncrement++
-
 $version = Get-Content "$PSScriptRoot\version.debug" -Raw
-
 $version = $version.Replace("{incremental}", $versionIncrement)
 
 Write-Host "Building PnP.Framework version $version"
 dotnet build $PSScriptRoot\..\src\lib\PnP.Framework\PnP.Framework.csproj --no-incremental /p:Version=$version
-
-Write-Host "Packinging PnP.Framework version $version"
-dotnet pack $PSScriptRoot\..\src\lib\PnP.Framework\PnP.Framework.csproj --no-build /p:PackageVersion=$version
-
-#Write-Host "Writing $version to git"
-#Set-Content -Path .\version.debug.increment -Value $versionIncrement
-
-#Push to the repo
-# git add .\version.debug.increment
-# git commit -m "Build increment - debug version $versionIncrement"
-# git push

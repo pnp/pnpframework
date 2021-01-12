7880ff8c054cfe92b7c6c9ea0c54987056059302
Bert Jansen <bjansen@microsoft.com>
2021-01-12 20:48:30 +0100

    Build script improvements

--- build/build-debug.ps1
+++ build/build-debug.ps1
@@ -1,6 +1,28 @@
+Param(
+	[Parameter(Mandatory = $false, ValueFromPipeline = $false)]
+	[switch]
+	$NoIncremental,
+	[Parameter(Mandatory = $false, ValueFromPipeline = $false)]
+	[switch]
+    $Force
+)
+
 $versionIncrement = Get-Content "$PSScriptRoot\version.debug.increment" -Raw
 $version = Get-Content "$PSScriptRoot\version.debug" -Raw
 $version = $version.Replace("{incremental}", $versionIncrement)
 
 Write-Host "Building PnP.Framework version $version"
-dotnet build $PSScriptRoot\..\src\lib\PnP.Framework\PnP.Framework.csproj --no-incremental /p:Version=$version
+
+$buildCmd = "dotnet build `"$PSScriptRoot/../src/lib/PnP.Framework/PnP.Framework.csproj`"" + "--nologo --configuration Debug -p:VersionPrefix=$version -p:VersionSuffix=debug";
+
+if ($NoIncremental) {
+	$buildCmd += " --no-incremental";
+}
+if ($Force) {
+	$buildCmd += " --force"
+}
+
+Write-Host "Executing $buildCmd" -ForegroundColor Yellow
+
+Invoke-Expression $buildCmd
+

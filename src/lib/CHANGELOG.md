c9c9e49b7914abd7505eab8e4ad69ec58b804945
Bert Jansen <bjansen@microsoft.com>
2021-01-22 13:08:29 +0100

    Fix for #110

--- src/lib/CHANGELOG.md
+++ src/lib/CHANGELOG.md
@@ -6,6 +6,16 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/).
 
+## [Nightly]
+
+### Added
+
+- Support for using the ICustomWebUi to enable interactive auth in PnP PowerShell [erwinvanhunen - Erwin van Hunen]
+
+### Changed
+
+- Fixed page transformation caching manager to handle dictionary serialization with non string value as keys. Fixes #110 [jansenbe - Bert Jansen]
+
 ## [1.1.0]
 
 ### Added
---
+++ src/lib/PnP.Framework/Modernization/Cache/DictionaryIntConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace PnP.Framework.Modernization.Cache
+{
+    internal class DictionaryIntConverter: JsonConverter<Dictionary<int, string>>
+    {
+        public override Dictionary<int, string> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.StartObject)
+            {
+                throw new JsonException();
+            }
+
+            var value = new Dictionary<int, string>();
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndObject)
+                {
+                    return value;
+                }
+
+                string keyString = reader.GetString();
+
+                if (!int.TryParse(keyString, out int keyAsInt32))
+                {
+                    throw new JsonException($"Unable to convert \"{keyString}\" to System.Int32.");
+                }
+
+                reader.Read();
+
+                string itemValue = reader.GetString();
+
+                value.Add(keyAsInt32, itemValue);
+            }
+
+            throw new JsonException("Error Occured");
+        }
+
+        public override void Write(Utf8JsonWriter writer, Dictionary<int, string> value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+
+            foreach (KeyValuePair<int, string> item in value)
+            {
+                writer.WriteString(item.Key.ToString(), item.Value);
+            }
+
+            writer.WriteEndObject();
+        }
+    }
+}

80bb2620f735110fd313991d977b1d2e5946a8c4
Koen Zomers <koen@zomers.eu>
2023-01-17 13:46:51 +0100

    Added optionally providing BCC addresses to send a mail

--- src/lib/PnP.Framework/Utilities/MailUtility.cs
+++ src/lib/PnP.Framework/Utilities/MailUtility.cs
@@ -12,7 +12,7 @@ namespace PnP.Framework.Utilities
     /// <summary>
     /// Provides functions for sending email using either Office 365 SMTP service or SharePoint's SendEmail utility
     /// </summary>
-    public class MailUtility
+    public static class MailUtility
     {
         /// <summary>
         /// Sends an email via Office 365 SMTP
@@ -28,8 +28,26 @@ namespace PnP.Framework.Utilities
         /// <param name="asyncUserToken">The user token that is used to correlate the asynchronous email message.</param>
         public static void SendEmail(string servername, string fromAddress, string fromUserPassword, IEnumerable<String> to, IEnumerable<String> cc, string subject, string body, bool sendAsync = false, object asyncUserToken = null)
         {
+            SendEmail(servername, fromAddress, fromUserPassword, to, cc, null, subject, body, sendAsync, asyncUserToken);
+        }
+
+        /// <summary>
+        /// Sends an email via Office 365 SMTP
+        /// </summary>
+        /// <param name="servername">Office 365 SMTP address. By default this is smtp.office365.com.</param>
+        /// <param name="fromAddress">The user setting up the SMTP connection. This user must have an EXO license.</param>
+        /// <param name="fromUserPassword">The password of the user.</param>
+        /// <param name="to">List of TO addresses.</param>
+        /// <param name="cc">List of CC addresses.</param>
+        /// <param name="bcc">List of BCC addresses.</param>
+        /// <param name="subject">Subject of the mail.</param>
+        /// <param name="body">HTML body of the mail.</param>
+        /// <param name="sendAsync">Sends the email asynchronous so as to not block the current thread (default: false).</param>
+        /// <param name="asyncUserToken">The user token that is used to correlate the asynchronous email message.</param>
+        public static void SendEmail(string servername, string fromAddress, string fromUserPassword, IEnumerable<String> to, IEnumerable<String> cc, IEnumerable<String> bcc, string subject, string body, bool sendAsync = false, object asyncUserToken = null)
+        {
             // Get the secure password
-            var secureString = new SecureString();
+            using var secureString = new SecureString();
             foreach (char c in fromUserPassword.ToCharArray())
             {
                 secureString.AppendChar(c);
@@ -52,8 +70,26 @@ namespace PnP.Framework.Utilities
         /// <param name="asyncUserToken">The user token that is used to correlate the asynchronous email message.</param>
         public static void SendEmail(string servername, string fromAddress, SecureString fromUserPassword, IEnumerable<String> to, IEnumerable<String> cc, string subject, string body, bool sendAsync = false, object asyncUserToken = null)
         {
+            SendEmail(servername, fromAddress, fromUserPassword, to, cc, null, subject, body, sendAsync, asyncUserToken);
+        }
+
+        /// <summary>
+        /// Sends an email via Office 365 SMTP
+        /// </summary>
+        /// <param name="servername">Office 365 SMTP address. By default this is smtp.office365.com.</param>
+        /// <param name="fromAddress">The user setting up the SMTP connection. This user must have an EXO license.</param>
+        /// <param name="fromUserPassword">The secure password of the user.</param>
+        /// <param name="to">List of TO addresses.</param>
+        /// <param name="cc">List of CC addresses.</param>
+        /// <param name="bcc">List of BCC addresses.</param>
+        /// <param name="subject">Subject of the mail.</param>
+        /// <param name="body">HTML body of the mail.</param>
+        /// <param name="sendAsync">Sends the email asynchronous so as to not block the current thread (default: false).</param>
+        /// <param name="asyncUserToken">The user token that is used to correlate the asynchronous email message.</param>
+        public static void SendEmail(string servername, string fromAddress, SecureString fromUserPassword, IEnumerable<String> to, IEnumerable<String> cc, IEnumerable<String> bcc, string subject, string body, bool sendAsync = false, object asyncUserToken = null)
+        {
             SmtpClient client = CreateSmtpClient(servername, fromAddress, fromUserPassword);
-            MailMessage mail = CreateMailMessage(fromAddress, to, cc, subject, body);
+            MailMessage mail = CreateMailMessage(fromAddress, to, cc, bcc, subject, body);
             try
             {
                 if (sendAsync)
@@ -99,6 +135,22 @@ namespace PnP.Framework.Utilities
         /// <param name="body">HTML body of the mail.</param>
         public static async Task SendEmailAsync(string servername, string fromAddress, string fromUserPassword, IEnumerable<String> to, IEnumerable<String> cc, string subject, string body)
         {
+            await SendEmailAsync(servername, fromAddress, fromUserPassword, to, cc, null, subject, body);
+        }
+
+        /// <summary>
+        /// Sends an email via Office 365 SMTP as an asynchronous operation
+        /// </summary>
+        /// <param name="servername">Office 365 SMTP address. By default this is smtp.office365.com.</param>
+        /// <param name="fromAddress">The user setting up the SMTP connection. This user must have an EXO license.</param>
+        /// <param name="fromUserPassword">The password of the user.</param>
+        /// <param name="to">List of TO addresses.</param>
+        /// <param name="cc">List of CC addresses.</param>
+        /// <param name="bcc">List of BCC addresses.</param>
+        /// <param name="subject">Subject of the mail.</param>
+        /// <param name="body">HTML body of the mail.</param>
+        public static async Task SendEmailAsync(string servername, string fromAddress, string fromUserPassword, IEnumerable<String> to, IEnumerable<String> cc, IEnumerable<String> bcc, string subject, string body)
+        {
             // Get the secure password
             var secureString = new SecureString();
             foreach (char c in fromUserPassword.ToCharArray())
@@ -106,7 +158,7 @@ namespace PnP.Framework.Utilities
                 secureString.AppendChar(c);
             }
 
-            await SendEmailAsync(servername, fromAddress, secureString, to, cc, subject, body);
+            await SendEmailAsync(servername, fromAddress, secureString, to, cc, bcc, subject, body);
         }
 
         /// <summary>
@@ -121,8 +173,24 @@ namespace PnP.Framework.Utilities
         /// <param name="body">HTML body of the mail.</param>
         public static async Task SendEmailAsync(string servername, string fromAddress, SecureString fromUserPassword, IEnumerable<String> to, IEnumerable<String> cc, string subject, string body)
         {
+            await SendEmailAsync(servername, fromAddress, fromUserPassword, to, cc, null, subject, body);
+        }
+
+        /// <summary>
+        /// Sends an email via Office 365 SMTP as an asynchronous operation
+        /// </summary>
+        /// <param name="servername">Office 365 SMTP address. By default this is smtp.office365.com.</param>
+        /// <param name="fromAddress">The user setting up the SMTP connection. This user must have an EXO license.</param>
+        /// <param name="fromUserPassword">The secure password of the user.</param>
+        /// <param name="to">List of TO addresses.</param>
+        /// <param name="cc">List of CC addresses.</param>
+        /// <param name="bcc">List of BCC addresses.</param>
+        /// <param name="subject">Subject of the mail.</param>
+        /// <param name="body">HTML body of the mail.</param>
+        public static async Task SendEmailAsync(string servername, string fromAddress, SecureString fromUserPassword, IEnumerable<String> to, IEnumerable<String> cc, IEnumerable<String> bcc, string subject, string body)
+        {
             SmtpClient client = CreateSmtpClient(servername, fromAddress, fromUserPassword);
-            MailMessage mail = CreateMailMessage(fromAddress, to, cc, subject, body);
+            MailMessage mail = CreateMailMessage(fromAddress, to, cc, bcc, subject, body);
             try
             {
                 await client.SendMailAsync(mail);
@@ -148,6 +216,20 @@ namespace PnP.Framework.Utilities
         /// <param name="body">HTML body of the mail.</param>
         public static void SendEmail(ClientContext context, IEnumerable<String> to, IEnumerable<String> cc, string subject, string body)
         {
+            SendEmail(context, to, cc, null, subject, body);
+        }
+
+        /// <summary>
+        /// Sends an email using the SharePoint SendEmail method
+        /// </summary>
+        /// <param name="context">Context for SharePoint objects and operations</param>
+        /// <param name="to">List of TO addresses.</param>
+        /// <param name="cc">List of CC addresses.</param>
+        /// <param name="bcc">List of BCC addresses.</param>
+        /// <param name="subject">Subject of the mail.</param>
+        /// <param name="body">HTML body of the mail.</param>
+        public static void SendEmail(ClientContext context, IEnumerable<String> to, IEnumerable<String> cc, IEnumerable<String> bcc, string subject, string body)
+        {
             EmailProperties properties = new EmailProperties
             {
                 To = to
@@ -157,6 +239,11 @@ namespace PnP.Framework.Utilities
             {
                 properties.CC = cc;
             }
+
+            if (bcc != null)
+            {
+                properties.BCC = bcc;
+            }            
 
             properties.Subject = subject;
             properties.Body = body;
@@ -190,7 +277,17 @@ namespace PnP.Framework.Utilities
             };
         }
 
-        private static MailMessage CreateMailMessage(string fromAddress, IEnumerable<String> to, IEnumerable<String> cc, string subject, string body)
+        /// <summary>
+        /// Constructs a MailMessage based on the provided parameters
+        /// </summary>
+        /// <param name="fromAddress">Address to use as the sender</param>
+        /// <param name="to">One or more recipients of the e-mail</param>
+        /// <param name="cc">Recipients to include as Carbon Copies in the e-mail</param>
+        /// <param name="bcc">Recipients to cinlude as Blind Carbon Copies in the e-mail</param>
+        /// <param name="subject">Subjec to use for the e-mail</param>
+        /// <param name="body">Contents of the e-mail</param>
+        /// <returns>MailMessage instance</returns>
+        private static MailMessage CreateMailMessage(string fromAddress, IEnumerable<String> to, IEnumerable<String> cc, IEnumerable<String> bcc, string subject, string body)
         {
             MailMessage mail = new MailMessage()
             {
@@ -203,13 +300,21 @@ namespace PnP.Framework.Utilities
             foreach (string user in to)
             {
                 mail.To.Add(user);
-            }
+            }          
 
             if (cc != null)
             {
                 foreach (string user in cc)
                 {
                     mail.CC.Add(user);
+                }
+            }
+
+            if (bcc != null)
+            {
+                foreach (string user in bcc)
+                {
+                    mail.Bcc.Add(user);
                 }
             }
 

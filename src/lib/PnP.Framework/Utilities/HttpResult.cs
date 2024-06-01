4cffde9ba9d1c5ff0a532838209c85abb0b6c766
David P <dpetchey@gmail.com>
2024-06-01 21:01:45 +0100

    Fix build issue

--- src/lib/PnP.Framework/Utilities/HttpResult.cs
+++ src/lib/PnP.Framework/Utilities/HttpResult.cs
@@ -2,5 +2,15 @@
 
 namespace PnP.Framework.Utilities
 {
-    public record HttpResult<T>(T Result, HttpResponseHeaders ResponseHeaders);
+    public class HttpResult<T>
+    {
+        public HttpResult(T Result, HttpResponseHeaders ResponseHeaders)
+        {
+            this.Result = Result;
+            this.ResponseHeaders = ResponseHeaders;
+        }
+
+        public T Result { get; }
+        public HttpResponseHeaders ResponseHeaders { get; }
+    }
 }
